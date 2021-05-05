5690fdd09e465f213f3c3892454e8a7bf095eee6
varenyaN <79528719+varenyaN@users.noreply.github.com>
2021-05-05 20:15:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var fixedRect, movingRect;
+var  car,wall;
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  car=createSprite(200,200,40,40);
+  car.shapeColor="blue";
+  wall=createSprite(1000,200,20,100);
+  wall.shapeColor="pink";
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+ if (isTouching(car,wall)){
+car.shapeColor="pink";
+ }
+ colide (car,wall);
+  drawSprites();
+}
